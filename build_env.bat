0cbfec687dcd9a953980f0b4c28aa504d691067e
彭大海 <dahai.pon@gmail.com>
2026-03-25 19:17:50 +0800

    chore: exclude auto-generated build_env.bat from git

--- build_env.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:: AUTO-GENERATED - DO NOT EDIT
-set "ARDUINO_CLI_PATH=C:\Program Files\Arduino IDE\resources\app\lib\backend\resources\arduino-cli.exe"
-set "PICOTOOL_PATH=C:\Users\pondahai\AppData\Local\Arduino15\packages\rp2040\tools\pqt-picotool\4.1.0-1aec55e\picotool.exe"
-set "ARDUINO_USER_LIB_PATH=c:\Users\pondahai\Dropbox\Arduino\libraries"
-set "PROJECT_ROOT=C:\Users\pondahai\Documents\GitHub\PicoApple2\"
-set "FQBN=rp2040:rp2040:rpipico"
